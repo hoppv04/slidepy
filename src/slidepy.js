3fdc6f74c3188e2bff340c9c39f80d0c21ccb5b9
hoppv <van.hop.pham@draphony.com>
2025-03-30 09:42:37 +0700

    Thêm option items và loop

--- src/slidepy.js
+++ src/slidepy.js
@@ -5,7 +5,12 @@ function Slidepy(selector, options = {}) {
     return;
   }
 
-  this.opt = { ...options };
+  this._defaultOpt = {
+    items: 1,
+    loop: false,
+  };
+
+  this.opt = { ...this._defaultOpt, ...options };
   this.slides = Array.from(this.container.children);
 
   this.currentIndex = 0;
@@ -27,6 +32,7 @@ Slidepy.prototype._createTrack = function () {
 
   this.slides.forEach((slide) => {
     slide.classList.add("slidepy-slide");
+    slide.style.flexBasis = `calc(100% / ${this.opt.items})`;
     this.track.appendChild(slide);
   });
 
@@ -50,10 +56,15 @@ Slidepy.prototype._createNavigation = function () {
 };
 
 Slidepy.prototype.moveSlide = function (step) {
-  this.currentIndex = Math.min(
-    Math.max(this.currentIndex + step, 0),
-    this.slides.length - 3
-  );
-  this.offset = -(this.currentIndex * (100 / 3));
+  if (this.opt.loop) {
+    this.currentIndex =
+      (this.currentIndex + step + this.slides.length) % this.slides.length;
+  } else {
+    this.currentIndex = Math.min(
+      Math.max(this.currentIndex + step, 0),
+      this.slides.length - this.opt.items
+    );
+  }
+  this.offset = -(this.currentIndex * (100 / this.opt.items));
   this.track.style.transform = `translateX(${this.offset}%)`;
 };
